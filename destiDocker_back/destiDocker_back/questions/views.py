b93fda21ab090a64ed1ad57660f5f251b94d2dae
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-06-09 21:09:42 -0400

    chore: Update questions page with continent and weather selection

--- destiDocker_back/destiDocker_back/questions/views.py
+++ destiDocker_back/destiDocker_back/questions/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 import requests
-import time
+import json
+import urllib
 
 # Create your views here.
 def index(request):
@@ -9,33 +10,30 @@ def index(request):
 def questions(request):
     if request.method == 'POST':
         continent = request.POST.get('continent')
+        weather = request.POST.get('weather')
         request.session['continent'] = continent
+        request.session['weather'] = weather
         return redirect('results')
     else: 
         return render(request, 'questions.html')
 
 def results(request):
     continent = request.session.get('continent')
-    
-    url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
-    
-    param = {
-        "query": f"cities in {continent}",
-        "key": "AIzaSyDJsxyUDBlOaUAalBvUdVrlw-DX-KZ-Jig"
-    }
-    
-    cities = []
-    
-    while True:
-        response = requests.get(url, params=param)
-        data = response.json()
-        cities.extend([result['name'] for result in data['results']])
-        if 'next_page_token' in data:
-            param['pagetoken'] = data['next_page_token']
-            
-            time.sleep(5)
-        else:
-            break
-    
-    context = {"cities": cities, "continent": continent}
-    return render(request, 'results.html', context)
+    weather = request.session.get('weather')
+
+    url = f'https://restcountries.com/v3.1/region/{continent.lower()}'
+
+    response = requests.get(url)
+    response.raise_for_status()  # Raises a HTTPError if the response status is 4xx, 5xx
+
+    data_continents = response.json()
+    countries = [country for country in data_continents]
+
+    if weather == 'hot':
+        filtered_countries = [country for country in countries if -23.5 <= country['latlng'][0] <= 23.5]
+    elif weather == 'cold':
+        filtered_countries = [country for country in countries if country['latlng'][0] < -66.5 or country['latlng'][0] > 66.5]
+    else:  # templado 
+        filtered_countries = [country for country in countries if -66.5 < country['latlng'][0] < 66.5 and not -23.5 <= country['latlng'][0] <= 23.5]
+
+    return render(request, 'results.html', {'continent': continent, 'data_continents': filtered_countries})

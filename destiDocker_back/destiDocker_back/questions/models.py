10ef21836d15ad91eed48635cccc4a4d6f3b7aca
freddyrosa16 <freddy.rosa16@gmail.com>
2024-06-26 14:11:26 -0400

    Update booking model to remove booking_date field and add paid field

--- destiDocker_back/destiDocker_back/questions/models.py
+++ destiDocker_back/destiDocker_back/questions/models.py
@@ -4,13 +4,13 @@ from django.contrib.auth.models import User
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    flight_number = models.CharField(max_length=20)
+    flight_number = models.CharField(max_length=100)
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField()
     departure_airport = models.CharField(max_length=100)
     arrival_airport = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    booking_date = models.DateTimeField(auto_now_add=True)
+    paid = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.user.username} - {self.flight_number}'
+        return f"Booking {self.flight_number} by {self.user.username}"

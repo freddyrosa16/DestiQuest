196d6e5c86115e7e83f129f2d58cb242da7b8b82
freddyrosa16 <freddy.rosa16@gmail.com>
2024-06-25 20:24:37 -0400

    Update login view in urls.py

--- destiDocker_back/destiDocker_back/destiDocker_back/urls.py
+++ destiDocker_back/destiDocker_back/destiDocker_back/urls.py
@@ -1,23 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-<<<<<<< HEAD
-from .views import RegisterView
-from django.conf import settings
-from django.conf.urls.static import static
-=======
 from .views import RegisterView, CustomLoginView, IATACodeAutocomplete
->>>>>>> recovery-branch
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', CustomLoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(template_name='register.html'),
          name='register'),
     path('get-iata-codes/', IATACodeAutocomplete.as_view(), name='get_iata_codes'),
     path('', include('questions.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

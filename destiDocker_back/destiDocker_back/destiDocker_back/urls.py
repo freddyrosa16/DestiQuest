b05abe1b5b52e14d1fa2d3ebb1aa2088cc766ff8
freddyrosa16 <freddy.rosa16@gmail.com>
2024-06-27 13:36:02 -0400

    Update urls.py to remove unnecessary code and improve readability

--- destiDocker_back/destiDocker_back/destiDocker_back/urls.py
+++ destiDocker_back/destiDocker_back/destiDocker_back/urls.py
@@ -1,5 +1,3 @@
-# ~/DestiQuest/destiDocker_back/destiDocker_back/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views

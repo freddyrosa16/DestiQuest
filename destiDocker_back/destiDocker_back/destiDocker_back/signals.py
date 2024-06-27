5c11580deb16b0dae856ec96372b145768e5a55d
freddyrosa16 <freddy.rosa16@gmail.com>
2024-06-27 18:36:59 -0400

    Update signals.py and flights.html template

--- destiDocker_back/destiDocker_back/destiDocker_back/signals.py
+++ destiDocker_back/destiDocker_back/destiDocker_back/signals.py
@@ -5,7 +5,7 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.contrib.auth.signals import user_logged_in
 
-
+"""
 @receiver(post_save, sender=User)
 def send_welcome_email(sender, instance, created, **kwargs):
     if created:
@@ -16,6 +16,7 @@ def send_welcome_email(sender, instance, created, **kwargs):
         )
         recipient_list = [instance.email]
         send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list)
+"""
 
 
 @receiver(user_logged_in)
